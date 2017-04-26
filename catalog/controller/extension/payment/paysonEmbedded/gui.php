5045235f460153ffa9ce765259099da7f7e126da
oscar-vil <oscar.villegas@payson.se>
2017-04-26 09:21:02 +0200

    Added countries.

--- catalog/controller/extension/payment/paysonEmbedded/gui.php
+++ catalog/controller/extension/payment/paysonEmbedded/gui.php
@@ -9,16 +9,19 @@ namespace PaysonEmbedded{
         public $verfication;
         /** @var bool $requestPhone  Can be used to require the user to fill in his phone number. */
         public $requestPhone;
+        /** @var list $countries  List of countries shown in the checkout snippet. */
+        public $countries = array();
         
-        public function __construct($locale = "sv", $colorScheme = "gray", $verfication = "none", $requestPhone = NULL){
+        public function __construct($locale = "sv", $colorScheme = "gray", $verfication = "none", $requestPhone = NULL, $countries = NULL){
             $this->colorScheme = $colorScheme;
             $this->locale = $locale; 
             $this->verfication = $verfication;
             $this->requestPhone = $requestPhone;
+            $this->countries = $countries;
         }
 
         public static function create($data) {
-            return new Gui($data->locale, $data->colorScheme, $data->verification, $data->requestPhone);
+            return new Gui($data->locale, $data->colorScheme, $data->verification, $data->requestPhone, $data->countries);
         }
         
         public function toArray(){
