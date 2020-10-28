38760248fff523b349b9d6ea6d143da23a07190c
oscar-vil <oscar.villegas@payson.se>
2020-10-28 15:25:47 +0100

    1.0.3.2
    Updated minimum amount

--- upload/catalog/controller/extension/payment/paysonCheckout2.php
+++ upload/catalog/controller/extension/payment/paysonCheckout2.php
@@ -4,7 +4,7 @@ class ControllerExtensionPaymentPaysonCheckout2 extends Controller {
     private $testMode;
     public $data = array();
 
-    const MODULE_VERSION = 'paysonEmbedded_1.0.3.1';
+    const MODULE_VERSION = 'paysonEmbedded_1.0.3.2';
 
     function __construct($registry) {
         parent::__construct($registry);
